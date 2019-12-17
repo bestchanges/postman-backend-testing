e4698bb781f8b40d98e2139bfbdf93e366bcbe61
Egor Fedorov <egor.fedorov@dataart.com>
2019-12-17 15:43:30 +0300

    schema updates

--- app.py
+++ app.py
@@ -11,37 +11,42 @@ def gen_id(data):
 
 global_todolists = {}
 
+def todolist_to_dto(todolist: dict):
+    todolist_dto = dict(todolist)
+    del todolist_dto['items']
+    return todolist_dto
 
 @app.route("/lists", methods=('GET',))
 def get_lists():
     lists = global_todolists.values()
-    return jsonify(list(lists))
+    return jsonify([todolist_to_dto(todolist) for todolist in lists])
 
 
 @app.route("/lists", methods=('POST',))
-def post_lists():
+def create_list():
     todolist = request.get_json()
     assert 'id' not in todolist
     list_id = gen_id(global_todolists)
     todolist['id'] = list_id
     todolist.setdefault('items', {})
     global_todolists[list_id] = todolist
-    return jsonify(todolist)
+    return jsonify(todolist_to_dto(todolist))
 
 
 @app.route("/lists/<int:list_id>", methods=('GET',))
 def get_list(list_id):
     todolist = global_todolists[list_id]
-    return jsonify(todolist)
+    return jsonify(todolist_to_dto(todolist))
 
 
 @app.route("/lists/<int:list_id>", methods=('PUT',))
-def put_lists(list_id):
+def update_list(list_id):
     todolist = request.get_json()
     assert todolist['id'] == list_id
+    assert list_id in global_todolists
     todolist.setdefault('items', {})
     global_todolists[list_id] = todolist
-    return jsonify(todolist)
+    return jsonify(todolist_to_dto(todolist))
 
 
 @app.route("/lists/<int:list_id>", methods=('DELETE',))
@@ -49,7 +54,7 @@ def delete_list(list_id):
     assert list_id in global_todolists
     todolist = global_todolists[list_id]
     del global_todolists[list_id]
-    return jsonify(todolist)
+    return jsonify(todolist_to_dto(todolist))
 
 
 @app.route("/lists/<int:list_id>/items", methods=('GET',))
@@ -59,12 +64,13 @@ def get_items(list_id: int):
 
 
 @app.route("/lists/<int:list_id>/items", methods=('POST',))
-def post_items(list_id: int):
+def create_item(list_id: int):
     item = request.get_json()
     assert 'id' not in item
     items = global_todolists[list_id]['items']
     item_id = gen_id(items)
     item['id'] = item_id
+    item.setdefault('completed', False)
     items[item_id] = item
     return jsonify(item)
 
@@ -78,12 +84,13 @@ def get_item(list_id: int, item_id: int):
 
 
 @app.route("/lists/<int:list_id>/items/<int:item_id>", methods=('PUT',))
-def put_item(list_id, item_id):
+def update_item(list_id, item_id):
     item = request.get_json()
     assert item['id'] == item_id
-    items = global_todolists[list_id]['items']
-    items[item_id] = item
-    return jsonify(item)
+    assert item_id in global_todolists[list_id]['items']
+    existing_item = global_todolists[list_id]['items'][item_id]
+    existing_item.update(item)
+    return jsonify(existing_item)
 
 
 @app.route("/lists/<int:list_id>/items/<int:item_id>", methods=('DELETE',))
